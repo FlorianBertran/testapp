ccced3fae1a4535a6f02d629bc1e68309105d106
Florian Bertrand <florian.bertrand@student.vives.be>
2019-01-29 16:17:29 +0100

    merge conflict solved

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-<<<<<<< HEAD
 //import App from './App';
 import App from "./Fien/App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-=======
-import App from "./components/App";
+/*import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import store from "./store/store";
 import { Provider } from "react-redux";
@@ -18,8 +16,7 @@ ReactDOM.render(
     <App />
   </Provider>,
   document.getElementById("root")
-);
->>>>>>> 3073fb9060714e0a59c8dbc891e36cf34e96e9d5
+);*/
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
